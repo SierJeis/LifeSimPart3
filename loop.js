6e345bb05befd514f3a8fe748c15fa0388a75c5c
Jeis <88003338+SierJeis@users.noreply.github.com>
2023-02-12 12:40:11 -0600

    final update

--- loop.js
+++ loop.js
@@ -1,12 +1,15 @@
 function updateCount(){
     setInterval(() => {
         document.querySelector("#learnspeed").innerHTML = "Learning Speed : "+learningspeed;
-        document.querySelector("#firstjob").innerHTML = "First Job : "+firstjoblevel;
+        document.querySelector("#firstjob").innerHTML = "Beggar : "+firstjoblevel;
         document.querySelector("#moneymulti").innerHTML = "Money Multiplier : "+moneylevel;
         document.querySelector("#learnfill").style.width = xp / xpreq * 100 + '%';
         document.querySelector("#jobfill").style.width = jobxp / jobxpreq * 100 + '%';
         document.querySelector("#moneyfill").style.width = moneyxp / moneyxpreq * 100 + '%';
-        document.querySelector(".inv").innerHTML = "G: "+Math.round(gold)+"<br>S: "+Math.round(silver)+"<br>C: "+Math.round(copper);
+        document.querySelector("#xpgain").innerHTML = Math.round(jobxpadd*multi);
+        document.querySelector("#xpreq").innerHTML = Math.round(xpreq);
+        document.querySelector("#firstjobincome").innerHTML = Math.round(income)+'c';
+        document.querySelector(".inv").innerHTML = "Income : "+Math.round(income)+"<br> Expenses : "+expense+"<br> G: "+Math.round(gold)+"<br>S: "+Math.round(silver)+"<br>C: "+Math.round(copper);
         if(xp >= xpreq) {
             xp = 0;
             learningspeed += 1;
@@ -25,16 +28,32 @@ function updateCount(){
             moneyxpreq *= 1.1;
             moneyskillmulti *= 1.05;
         }
-        if(copper >= 100){
+        if(copper >= 101){
             copper = 0;
             silver += 1;
         }
-        if(silver >= 100){
+        if(silver >= 101){
             silver = 0;
             gold += 1;
+        }
+        if(copper <= 0){
+            silver -= 1;
+            copper += 100;
         }
         if(learningspeed >= 1){
             document.querySelector(".moneydiv").style.display = 'inline';
         }
+        if(houseBought == true) {
+            income -= houseCost;
+        }
+        income = (1*firstjobmulti*moneyskillmulti)+expense;
     }, 10);
+    setInterval(() => {
+        copper += income;
+        if(copper <= 0){
+            goBankrupt();
+        }
+    }, 1000);
+    filljob()
+    filllearn();
 }
--- main.js
+++ main.js
@@ -5,9 +5,10 @@ var multi = 1.02
 var xp = 10;
 var xpreq = 100;
 var xpadd = 1;
+let houseCost = 10;
 var gold = 0;
 var silver = 0;
-var copper = 0;
+var copper = 10;
 var jobxp = 10;
 var jobxpadd = 1;
 var jobxpreq = 100;
@@ -23,14 +24,20 @@ var currentskill = null;
 const jobbtn = document.getElementById("jobbar");
 const learnspeedbtn = document.getElementById("learnbar");
 const moneymultbtn = document.getElementById("moneybar");
+const housebtn = document.getElementById("house");
+let income = 0;
+let expense = 0;
+let houseBought = false;
+let globalmulti = 1;
 
 jobbtn.addEventListener("click", filljob);
 learnspeedbtn.addEventListener("click", filllearn);
 moneymultbtn.addEventListener("click", moneyfill);
+housebtn.addEventListener("click", buyHouse);
 
 function filllearn(){
        fillint = setInterval(() => {
-            xp += xpadd*multi
+            xp += xpadd*multi*globalmulti
         }, 10);
         learnspeedbtn.removeEventListener("click", filllearn);
         clearInterval(moneyint);
@@ -40,28 +47,58 @@ function filllearn(){
 function switchskills(){
     document.querySelector(".jobs").style.display = 'none';
     document.querySelector(".skills").style.display = 'inline';
+    document.querySelector(".shop").style.display = 'none';
 }
 
 function switchjobs(){
     document.querySelector(".jobs").style.display = 'inline';
     document.querySelector(".skills").style.display = 'none';
+    document.querySelector(".shop").style.display = 'none';
+}
+
+function switchshop(){
+    document.querySelector(".jobs").style.display = 'none';
+    document.querySelector(".skills").style.display = 'none';
+    document.querySelector(".shop").style.display = 'inline';
 }
 
 function filljob(){
     setInterval(() => {
-        jobxp += jobxpadd*multi
+        jobxp += jobxpadd*multi*globalmulti
     }, 10);
-    setInterval(() => {
-        copper += 1*multi;
-    }, 1000);
     jobbtn.removeEventListener("click", filljob);
 }
 
 function moneyfill(){
         moneyint = setInterval(() => {
-            moneyxp += moneyxpadd*multi
+            moneyxp += moneyxpadd*multi*globalmulti
         }, 10);
         moneymultbtn.removeEventListener("click", moneyfill);
         clearInterval(fillint);
         learnspeedbtn.addEventListener("click", filllearn);
+}
+
+function buyHouse(){
+    if(copper >= houseCost) {
+            houseBought = true;
+            expense -= houseCost;
+        document.querySelector("#housedot").style.backgroundColor = 'green';
+        globalmulti = 1.1;
+        housebtn.removeEventListener("click", buyHouse);
+        housebtn.addEventListener("click", sellHouse);
+    }
+}
+
+function sellHouse(){
+    document.querySelector("#housedot").style.backgroundColor = 'red';
+    houseBought = false;
+    expense += 10;
+    housebtn.removeEventListener("click", sellHouse);
+    housebtn.addEventListener("click", buyHouse);
+}
+
+function goBankrupt(){
+    if(copper <= 0){
+        sellHouse()
+    }
 }
